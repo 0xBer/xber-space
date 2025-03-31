5e50dcaa6f8552265fbf265f74d953cd94ab8a8a
0xBer <vol.gre13@gmail.com>
2025-03-31 21:47:28 +0300

    Refactor

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -4,7 +4,7 @@ const router = e.Router();
 
 router.get('/', User.getAllUsers);
 router.get('/:id', User.getOneUser);
-router.patch('/:id');
+router.patch('/:id', User.updateUser);
 router.delete('/:id', User.deleteUser);
 
 export default router;
