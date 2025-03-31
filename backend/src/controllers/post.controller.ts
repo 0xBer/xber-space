25406167cfd1f61295d6913d21417706672dc981
0xBer <vol.gre13@gmail.com>
2025-03-31 15:10:17 +0300

    Post CR

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -1,8 +1,50 @@
 import { RequestHandler } from "express";
+import pool from "../db.js";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const SECRET = process.env.SECRET as string;
+
+export const getAllPosts: RequestHandler = async (req, res) => {
+    try {
+        const posts = await pool.query('SELECT * FROM posts');
+        if (posts.rows.length === 0) {
+            return res.status(401).json({ message: "No posts found" });
+        }
+        return res.status(200).json(posts.rows);
+    } catch (error) {
+        return res.status(400).json({ message: "Cant get posts" });
+    }
+};
+
+export const getOnePost: RequestHandler = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const post = await pool.query('SELECT * FROM posts WHERE id = $1', [id]);
+
+        if (post.rows.length === 0) {
+            return res.status(401).json({ message: "No posts found" });
+        }
+
+        return res.status(200).json(post.rows);
+    } catch (error) {
+        return res.status(400).json({ message: "Cant get posts" });
+    }
+};
 
 export const createPost: RequestHandler = async (req, res) => {
     try {
+        const { title, body } = req.body;
 
+        const token = req.headers.authorization as string;
+        const decoded = jwt.verify(token, SECRET) as JwtPayload;
+        const author = decoded.username;
+
+        const post = await pool.query('INSERT INTO posts (title, body, author) VALUES ($1, $2, $3) RETURNING *', [title, body, author]);
+
+        return res.status(200).json(post.rows);
     } catch (error) {
         return res.status(400).json({ message: "Cant create post" });
     }
--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -1,7 +1,9 @@
 import e from "express";
-
+import * as Posts from "../controllers/post.controller.js";
 const router = e.Router();
 
-router.get('/');
+router.get('/', Posts.getAllPosts);
+router.get('/:id', Posts.getOnePost);
+router.post('/', Posts.createPost);
 
 export default router;
