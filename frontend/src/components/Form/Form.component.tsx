69644dc97d5348e017e38955741d16d0e8c07355
0xBer <vol.gre13@gmail.com>
2025-04-04 23:45:57 +0300

    showPass checkbox

--- frontend/src/components/Form/Form.component.tsx
+++ frontend/src/components/Form/Form.component.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styles from "./Form.module.scss";
 
 function Form() {
+    const [pass, setPass] = useState(true);
+
     return (
         <>
             <form className={styles.form}>
@@ -8,7 +11,11 @@ function Form() {
                     <label htmlFor="mailorname">Name/email</label>
                     <input type="text" id="mailorname" />
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
+                    <input type={pass ? 'password' : 'text'} id="password" />
+                    <div>
+                        <input onClick={() => setPass(!pass)} type="checkbox" name="type" id="setType" />
+                        <label htmlFor="setType">Show password</label>
+                    </div>
                 </div>
                 <div className={styles.buttonWrapper}>
                     <button>Login</button>
