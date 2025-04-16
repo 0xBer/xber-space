b2d0c63ebe3b6fd5f601fcef950e98f087547034
0xBer <vol.gre13@gmail.com>
2025-04-16 22:48:58 +0300

    login functionality

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,16 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/autentication.route.js";
 import { authCheck } from "./middlewares/authorization.middleware.js";
 import postRouter from "./routes/post.route.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use('/auth', authRouter);
+
 app.use('/users', authCheck, userRouter);
 app.use('/posts', authCheck, postRouter);
 
--- frontend/src/pages/LoginForm/Login.page.tsx
+++ frontend/src/pages/LoginForm/Login.page.tsx
@@ -1,16 +1,42 @@
 import "./Login.modules.scss";
+import axios from "../../axios";
+import { useState } from "react";
 
 function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPass] = useState('');
+
+    const handleFormSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        try {
+            const response = axios.post("/auth/login", {
+                username,
+                password: password,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
             <div className="form">
                 <h1>
                     Xber
                 </h1>
-                <form className="loginForm">
-                    <input type="text" placeholder="Username, email" required />
-                    <input type="password" placeholder="Password" required />
-                    <button>Log in</button>
+                <form className="loginForm" onSubmit={handleFormSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Username, email"
+                        required
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        required value={password}
+                        onChange={(e) => setPass(e.target.value)} />
+                    <input type="submit" value="Log in"></input>
                 </form>
                 <div className="orDiv">
                     <hr />
