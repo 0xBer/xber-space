842fa4cf006b03e148caed0fd32723a6ad49980a
0xBer <vol.gre13@gmail.com>
2025-04-17 23:59:24 +0300

    small fixes

--- frontend/src/pages/LoginForm/Login.page.tsx
+++ frontend/src/pages/LoginForm/Login.page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPass] = useState('');
+    const [isLogged, setIsLogged] = useState(false);
 
     const handleFormSubmit = (e: React.SyntheticEvent) => {
         e.preventDefault();
@@ -13,8 +14,10 @@ function LoginPage() {
                 username,
                 password,
             });
+
             console.log(response)
         } catch (error) {
+            setVisibility(true);
             console.error(error);
         }
     }
